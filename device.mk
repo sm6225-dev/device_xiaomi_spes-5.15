aff336918571901d4d6bb6311c54d02d4696b076
Arvind Kumar <quic_c_karvin@quicinc.com>
2022-09-20 15:42:10 +0530

    spes: Change fastboot hal version to 1.1
    Updated the fastboot HAL to be using 1.1 for bengal_515.
    
    Change-Id: I86d024446f5519b1d2a805f7509d2458784c25b3
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
 # Fingerprint
