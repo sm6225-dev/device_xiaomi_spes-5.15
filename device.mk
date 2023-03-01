f0a4a960439072acca4793d7df7a7d19581aaec2
Cyber Knight <cyberknight755@gmail.com>
2023-03-02 00:19:24 +0800

    spes: Support Doze Single/Double Tap Sensors
    * AOSPA/android_device_xiaomi_sunny@97dcb58
    * AOSPA/android_device_xiaomi_sunny@d6c5d56
    * AOSPA/android_device_xiaomi_sunny@2bfc290
    * AOSPA/android_device_xiaomi_sunny@98fca00
    * AOSPA/android_device_xiaomi_sunny@e06fa26
    
    Change-Id: I71aa46aef7a4b6d79e6664968d0ee1c0f8866f9d
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -501,6 +501,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
