47fa9d3b0feb5458a3acd039ffe65eef816c49fb
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    spes: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a
    Signed-off-by: Guidix <guidixm@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,19 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+        "device/xiaomi/spes",
+        "hardware/qcom-caf/sm6225",
+        "hardware/qcom-caf/wlan",
+        "hardware/xiaomi",
+        "vendor/qcom/opensource/commonsys/display",
+        "vendor/qcom/opensource/commonsys-intf/display",
+        "vendor/qcom/opensource/dataservices",
+        "vendor/qcom/opensource/display",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
