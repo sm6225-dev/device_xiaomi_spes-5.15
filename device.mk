f3004a79892b0dca462e55bf8de1d0eb57e05e87
Tim Zimmermann <tim@linux4.de>
2024-01-21 20:08:37 +0100

    spes: Switch to AIDL Fingerprint HAL
    - As of Android 15, AIDL Fingerprint HAL is required to set fingerprint icon indicator pos.
    
    Change-Id: Ic5bb3ed213bbdfe147c863aa7258bde4731e06dd
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.common-V2-ndk.vendor \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     android.hardware.biometrics.fingerprint-V2-ndk.vendor \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     com.fingerprints.extension@1.0.vendor \
