7615b2d89da31e4a00df963f718f1115da58a3bd
muralivijay <muralidharan9845@gmail.com>
2024-05-16 01:12:08 +0530

    spes: Import libinit from 4.19 tree
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    
    Change-Id: I0e8e44f89d6c9ec5ad4f139d0871d633cff7531a
    Signed-off-by: Guidix <guidixm@gmail.com>
    Co-Authored-By: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,10 @@ TARGET_MERGE_DTBS_WILDCARD ?= khaje
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_spes
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_spes
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
