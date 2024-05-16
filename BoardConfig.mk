dfe4f5cf06201db4cae4b157b1dced2062b50b93
boedhack99 <boedhack@gmail.com>
2024-05-16 15:31:59 +0530

    spes: Disable NFC for spes with sku
    Ref:
    * https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common/blob/lineage-19.1/rootdir/etc/init.nfc.rc
    
    * ArrowOS-Devices/android_device_xiaomi_raphael@3cfceb4
    
    [Jabiyeff: Adapt NFC for spesn]
    Co-Authored-By: Jabiyeff <cebiyevanar@gmail.com>
    [muralivijay: Adapt changes for 5.15]
    Co-Authored-By: muralivijay <muralidharan9845@gmail.com>
    
    Change-Id: Ib760c2e23b283a93fc19f21d583e420fb47dcd6f
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -254,6 +254,8 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/network_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += k7tn
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
