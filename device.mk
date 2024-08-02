f1437e45a56ee4e765ead3f272515b25f1ba9de4
Rafiester <rafi.haqqi@gmail.com>
2024-08-02 21:44:33 +0530

    spes: Don't build qcom power hal
    Add TARGET_PROVIDES_POWERHAL := true
    to avoid conflict with caf powerhal
    
    Change-Id: I99f1b27559716d771b5dc746b99643c4aca92d6b
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -497,6 +497,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+TARGET_PROVIDES_POWERHAL := true
+
 # QMI
 PRODUCT_PACKAGES += \
     libcurl.vendor \
