372da19ff45ffd5bf1fc91a701ce6f54a567a6a1
muralivijay <muralidharan9845@gmail.com>
2024-10-05 23:55:10 +0530

    spes: Move bootconfig parameters to kernel cmdline.
    * Spes bootloader don,t support bootconfig thats why.
    
    Change-Id: I493968f3c32f2fd33f73a920e87e416197bcc862
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,14 +109,12 @@ BOARD_RAMDISK_USE_LZ4 := true
 BOARD_INIT_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
-BOARD_BOOTCONFIG := \
+BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=4e00000.dwc3 \
     androidboot.selinux=permissive
-
-BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
