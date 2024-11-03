f781facdbc8a214d10c18199d65a47e3ff48794f
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:33:51 +0200

    spes: Switch to python extract-utils
    Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9
    Signed-off-by: Guidix <guidixm@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/spes',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'hardware/lineage/interfaces/power-libperfmgr',
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/google/interfaces',
+    'hardware/google/pixel',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_vendor' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.dpmservice@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccsyshal@1.2',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ImsRtpService-V1-ndk',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+        'vendor.qti.qccvndhal_aidl-V1-ndk',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libagmclient',
+        'libpalclient',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .sig_replace('13 0A 00 94', '1F 20 03 D5'),
+    'vendor/lib64/camera/components/com.qti.node.mialgocontrol.so': blob_fixup()
+        .strip_debug_sections()
+        .add_needed('libpiex_shim.so'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .strip_debug_sections()
+        .add_needed('libcamxmmaphook.so'),
+    'vendor/bin/hw/vendor.qti.hardware.display.composer-service': blob_fixup()
+        .sig_replace('FF 03 01 D1 FD 7B 01 A9 FD 43 00 91 F5 13 00 F9', 'C0 03 5F D6 FD 7B 01 A9 FD 43 00 91 F5 13 00 F9')
+        .replace_needed('vendor.xiaomi.hardware.displayfeature@1.0.so', 'libvendor.xiaomi.hardware.displayfeature@1.0.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+    (
+        'vendor/bin/hw/vendor.xiaomi.hardware.displayfeature@1.0-service',
+        'vendor/lib64/hw/vendor.xiaomi.hardware.displayfeature@1.0-impl.so',
+        'vendor/lib64/libdisplayfeatureservice.so',
+        'vendor/lib64/libsdmcore.so',
+    ): blob_fixup()
+        .replace_needed('vendor.xiaomi.hardware.displayfeature@1.0.so', 'libvendor.xiaomi.hardware.displayfeature@1.0.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'spes',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
