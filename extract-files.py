5a074bd2e6a5c14a34918bc0342787f1de034caf
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    spes: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270
    Signed-off-by: Guidix <guidixm@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -52,8 +51,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.1',
         'vendor.qti.qccvndhal_aidl-V1-ndk',
     ): lib_fixup_vendor_suffix,
-    (
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
