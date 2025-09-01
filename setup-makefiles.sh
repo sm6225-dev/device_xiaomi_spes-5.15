c0284bd9f3ba1de6c595e4ad167e61d3dc387657
dodyirawan85 <dev.irawans@gmail.com>
2025-09-01 12:06:20 +0700

    spes: Import missing dpm blobs
    Change-Id: I56e5e81d11a9dd845bfa437b914fdbb3cd4f89c2
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,6 +47,7 @@ function lib_to_package_fixup_vendor_variants() {
         com.qualcomm.qti.dpm.api@1.0 | \
             vendor.qti.diaghal@1.0 | \
             vendor.qti.hardware.fm@1.0 | \
+            vendor.qti.hardware.dpmservice@1.0 | \
             vendor.qti.hardware.qccsyshal@1.0 | \
             vendor.qti.hardware.qccsyshal@1.1 | \
             vendor.qti.hardware.qccsyshal@1.2 | \
