7016977a0097432f65d8553293555ac0732569b6
Guidix <guidixm@gmail.com>
2026-06-19 21:41:42 +0300

    spes: Initial bengal_515 tree for spes/n
    Signed-off-by: Guidix <guidixm@gmail.com>
    
    q

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_spes.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_spes-user \
+    lineage_spes-userdebug \
+    lineage_spes-eng
---
+++ lineage_spes.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from spes device
+$(call inherit-product, device/xiaomi/spes/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_spes
+PRODUCT_DEVICE := spes
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 11
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Device Configs
+TARGET_BOOT_ANIMATION_RES := 1080
