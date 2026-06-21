muralivijay/android_device_xiaomi_spes-5.15

spes: Update ODM manifest flag for K7TN
Signed-off-by: muralivijay <muralidharan9845@gmail.com>

BoardConfig.mk
@@ -273,7 +273,7 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/network_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += k7tn
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
+ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn

sm6225-dev/device_xiaomi_spes-5.15

Change-Id: I5657c6f4cedd0f1d11e3c9c386859b973ff27d05
Signed-off-by: Guidix <guidixm@gmail.com>

BoardConfig.mk
@@ -272,7 +272,7 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/network_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += k7tn
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
+ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/vintf/manifest_k7tn.xml
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn