8d81abb085ba17ccc4b9c7c790b5f076629b5ca0
GuidixX <guidixm@gmail.com>
2026-06-29 15:56:59 +0300

    spes: Let's go enforcing
    This reverts commit eca5b3d02b06d86a562cee7e4e77c581c77e895c.
    
    Change-Id: I581dc8e8814be5efa9d182f96200a65b8ccd48b8
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,8 +113,7 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=4e00000.dwc3 \
-    androidboot.selinux=permissive
+    androidboot.usbcontroller=4e00000.dwc3
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
