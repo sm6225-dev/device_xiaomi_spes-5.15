db3e780dbd8859d95c9aa2cfd4bc9d7ee7c0729b
GuidixX <guidixm@gmail.com>
2026-06-29 15:13:35 +0300

    spes: sensors: Rename to avoid conflicts
    Change-Id: I0f6a623ae59046187f5705ee3051b2522c600211
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -502,7 +502,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi.spes
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
