7f12d5873e2f79ed7d84c0c681cdb0827d07dbed
Guidix <guidixm@gmail.com>
2026-06-29 18:45:46 +0300

    spes: Build android.hardware.media.c2@1.0 from source
    Change-Id: I1af6355421c270545e6c41bd1c1163023ef2af27
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -394,6 +394,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
