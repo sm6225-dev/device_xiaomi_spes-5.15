d146b6acded7cd94f8d7a1de4b19e1a9441f9e82
GuidixX <guidixm@gmail.com>
2026-06-30 19:00:24 +0300

    spes: Drop libstdc++
    Change-Id: I81af048562b4fd625d7dbb1b9cdc94de8363b437

--- device.mk
+++ device.mk
@@ -167,7 +167,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    libstdc++.vendor \
     libpiex_shim \
     libcamxmmaphook \
     libcamxexternalformatutils \
