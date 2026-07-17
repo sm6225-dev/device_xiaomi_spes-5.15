6466053ab1d8abb4b1fe2c643f9e4351fd556ac3
HALt <102590462+halt-spesn@users.noreply.github.com>
2026-07-17 21:42:57 +0300

    spes: build lineage livedisplay hal
    Change-Id: I3ad5020798c06095eaf8c9c6592613b16da7828f
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -303,6 +303,15 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm \
+    vendor.lineage.livedisplay-service.sysfs
+
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set_bool,livedisplay_sysfs,enable_af,true)
+$(call soong_config_set_bool,livedisplay_sysfs,enable_se,true)
+
 # Media
 PRODUCT_PACKAGES += \
     libsfplugin_ccodec_utils.vendor \
